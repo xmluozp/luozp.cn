85bc342c88575b1011b85bf06d13a30f2b316dde
xmluozp <zhaoping.luo.winnipeg@gmail.com>
2019-10-06 15:46:01 -0500

    fix bug: flickering when finish entering

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './App.module.scss';
+import './App.scss';
 import { BrowserView, MobileView } from 'react-device-detect';
 import { Link, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
@@ -16,11 +17,11 @@ function App(props) {
   const [routeAnimationFinished, setRouteAnimationFinished] = useState(true);
   const [navLocks, setNavLocks] = useState([]);
   const navLinks = [
-    {title: "Home", to: "/"},
-    {title: "Skill Sets", to: "/techstack"},
-    {title: "Personal", to: "/aboutme"},
-    {title: "Blog", to: "/blog"},
-    {title: "About", to: "/aboutwebsite"},
+    { title: "Home", to: "/" },
+    { title: "Skill Sets", to: "/techstack" },
+    { title: "Personal", to: "/aboutme" },
+    { title: "Blog", to: "/blog" },
+    { title: "About", to: "/aboutwebsite" },
   ];
 
 
@@ -29,6 +30,7 @@ function App(props) {
   // have a lock list. in order to prevent messing up page animations when hitting nav items too quick
   const handleLockNav = (e) => {
 
+    console.log(e);
     setRouteAnimationFinished(false);
     const currentLocks = navLocks.slice();
     if (!currentLocks.includes(e.getAttribute('data-path'))) currentLocks.push(e.getAttribute('data-path'));
@@ -41,14 +43,14 @@ function App(props) {
     clearTimeout(timeoutCleanup);
     setTimeout(() => {
       timeoutCleanup = setRouteAnimationFinished(true);
-    }, timeout / 2);
+    }, timeout/2);
   }
 
 
   // Main JSX
   return (
     <>
-      <Nav navLocks={navLocks} navLinks={navLinks}/>
+      <Nav navLocks={navLocks} navLinks={navLinks} />
       {/* section */}
       <section className={`z_main ${styles.main} ${routeAnimationFinished ? styles.main_unlock : styles.main_lock}`}>
         <AnimationRoute path="/" exact component={Home} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
@@ -65,32 +67,74 @@ export default withRouter(App);
 
 //---------------------------------Wrapper for animation
 // Animation for switch pages
-const AnimationRoute = withRouter((props) => {
+const AnimationRouteTest = withRouter((props) => {
 
-  const timeout = 500;
+  const timeout = 5000;
 
   const clearup = () => {
     props.onCleanUp(timeout / 2);
   }
 
   return (
+
+    <div className="slide-in-exit-active" data-path={props.path}>
+      <Route {...props} />
+
+
+
+
+      {/* <CSSTransition
+          classNames="slide-in"
+          timeout={timeout}
+          in={props.location.pathname === props.path}
+          unmountOnExit
+          onExit={props.onLock}
+          onExited={props.onUnlock}
+          onEnter={props.onLock}
+          onEntered={(e) => { props.onUnlock(e); clearup() }}
+          appear={true}
+        >
+          <div className={props.path} data-path={props.path}>
+            <Route {...props} />
+          </div>
+
+        </CSSTransition> */}
+    </div>
+
+  );
+})
+
+
+// Animation for switch pages
+const AnimationRoute = withRouter((props) => {
+
+  const timeout = 2500;
+
+  const clearup = () => {
+    props.onCleanUp(timeout / 2);
+  }
+
+  // "Transition group" because we want to temporary keep the page while its exiting.
+  // here is "key" attribute, CSSTransition is "in" attribute
+  return (
     <TransitionGroup>
       <CSSTransition
         classNames="slide-in"
         timeout={timeout}
-        key={props.location.pathname}
-        unmountOnExit
+        key={props.location.pathname === props.path}
+
         onExit={props.onLock}
         onExited={props.onUnlock}
         onEnter={props.onLock}
         onEntered={(e) => { props.onUnlock(e); clearup() }}
         appear={true}
       >
-        <div className={props.location.pathname} data-path={props.location.pathname}>
+        <div className={props.path} data-path={props.path}>
           <Route {...props} />
         </div>
 
       </CSSTransition>
-    </TransitionGroup >
+
+    </TransitionGroup>
   );
 })
