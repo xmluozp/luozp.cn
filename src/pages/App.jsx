1aa49d5cb492be0523907271b135baa7abd2c6dc
xmluozp <zhaoping.luo.winnipeg@gmail.com>
2019-10-06 10:02:57 -0500

    fix bug: when hitting too quick, extend the clean up timeout

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -13,28 +13,41 @@ import TechStack from './TechStack/TechStack';
 function App(props) {
 
   const { location } = props;
+  const [routeAnimationFinished, setRouteAnimationFinished] = useState(true);
   const [navLocks, setNavLocks] = useState([]);
-  const [navDisplayItems, setNavDisplayItems] = useState(false);
+  const [navDisplayItems, setNavDisplayItems] = useState(true);
+  let timeoutCleanup;
 
+  // have a lock list. in order to prevent messing up page animations when hitting nav items too quick
   const handleLockNav = (e) => {
+
+    setRouteAnimationFinished(false);
     const currentLocks = navLocks.slice();
     if (!currentLocks.includes(e.getAttribute('data-path'))) currentLocks.push(e.getAttribute('data-path'));
     setNavLocks(currentLocks);
   }
   const handleUnlockNav = (e) => {
-    let currentLocks = navLocks.slice();
-    // currentLocks = currentLocks.filter(v=>v!==e.getAttribute('data-path'));
     setNavLocks([]);
   }
-  const handleToggleMenu = (e) =>{
-    if (location.pathname !== '/')
-     {
-       setNavDisplayItems(!navDisplayItems);
-       e.preventDefault(); 
-     }
-     else{
-      setNavDisplayItems(false);
-     }
+
+  // 
+  const handleCleanUp = (timeout) => {
+    clearTimeout(timeoutCleanup);
+    setTimeout(() => {
+      timeoutCleanup = setRouteAnimationFinished(true);
+    }, timeout / 2);
+  }
+
+  // mobile only: toggle menu
+  const handleToggleMenu = (e) => {
+    if (location.pathname !== '/') {
+      setNavDisplayItems(!navDisplayItems);
+      e.preventDefault();
+    }
+    else {
+      // on mobile, set the default nav display: true
+      // setNavDisplayItems(false);
+    }
   }
 
 
@@ -43,50 +56,56 @@ function App(props) {
     <>
       {/* header */}
       <BrowserView>
+        {/* check if display icon as the avatar(entrance) or a nav(inner pages) */}
         <header className={`${styles.header} ${location.pathname === '/' ? styles.nav_entrance : styles.nav_side}`}>
           <div className={styles.animation_menu}>
+            {/* if its entrance, hit the avatar will go to: [about me] */}
             <AnimationLink to={location.pathname === '/' ? "/aboutme" : "/"} lock={navLocks} >
               <div className={styles.avatar}></div>
             </AnimationLink>
-            <div className={styles.navitems}>
-              <NavItems navLocks={navLocks} />
-            </div>
+
+            <NavItems navLocks={navLocks} />
+
           </div>
         </header>
       </BrowserView>
 
       <MobileView>
+        {/* check if display icon as the avatar(entrance) or a mobile nav(inner pages) */}
         <header className={`${styles.header} ${location.pathname === '/' ? styles.nav_entrance : styles.nav_mobileside}`}>
+
+          {/* if its inner pages, hit the avatar will display the nav*/}
           <Link to="/aboutme" onClick={handleToggleMenu}>
             <div className={styles.avatar}></div>
           </Link>
-          <div className={navDisplayItems ? styles.navitems_display:styles.navitems_hide}>
-            <div className={styles.navitems}>
-              <NavItems navLocks={navLocks} />
-            </div>
+
+          {/* display the nav */}
+          <div className={navDisplayItems ? styles.navitems_display : styles.navitems_hide}>
+            <NavItems navLocks={navLocks} />
           </div>
         </header>
       </MobileView>
 
 
       {/* section */}
-      <section className={styles.main}>
-        <AnimationRoute path="/" exact component={Home} onLock={handleLockNav} onUnlock={handleUnlockNav} />
-        <AnimationRoute path="/aboutme" exact component={AboutMe} onLock={handleLockNav} onUnlock={handleUnlockNav} />
-        <AnimationRoute path="/blog" exact component={Blog} onLock={handleLockNav} onUnlock={handleUnlockNav} />
-        <AnimationRoute path="/techstack" exact component={TechStack} onLock={handleLockNav} onUnlock={handleUnlockNav} />
-        <AnimationRoute path="/aboutwebsite" exact component={AboutWebsite} onLock={handleLockNav} onUnlock={handleUnlockNav} />
+      <section className={`${styles.main} ${routeAnimationFinished ? styles.main_unlock : styles.main_lock}`}>
+        <AnimationRoute path="/" exact component={Home} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
+        <AnimationRoute path="/aboutme" exact component={AboutMe} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
+        <AnimationRoute path="/blog" exact component={Blog} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
+        <AnimationRoute path="/techstack" exact component={TechStack} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
+        <AnimationRoute path="/aboutwebsite" exact component={AboutWebsite} onLock={handleLockNav} onUnlock={handleUnlockNav} onCleanUp={handleCleanUp} />
       </section>
     </>
   );
 }
 
 export default withRouter(App);
-//---------------------------------Wrapper for nav
+
+//---------------------------------Wrapper for nav links
 const NavItems = withRouter((props) => {
   const { navLocks } = props
   return (
-
+    <div className={styles.navitems}>
       <ul>
         <li>
           <AnimationLink to="/" lock={navLocks} >
@@ -114,31 +133,39 @@ const NavItems = withRouter((props) => {
                 </AnimationLink>
         </li>
       </ul>
-     )
-
+    </div>
+  )
 })
 
 //---------------------------------Wrapper for animation
 // Animation for switch pages
 const AnimationRoute = withRouter((props) => {
 
+  const timeout = 500;
+
+  const clearup = () => {
+    props.onCleanUp(timeout / 2);
+  }
+
   return (
     <TransitionGroup>
       <CSSTransition
         classNames="slide-in"
-        timeout={500}
+        timeout={timeout}
         key={props.location.pathname}
         unmountOnExit
         onExit={props.onLock}
         onExited={props.onUnlock}
+        onEnter={props.onLock}
+        onEntered={(e)=>{props.onUnlock(e); clearup()}}
         appear={true}
       >
         <div className={props.location.pathname} data-path={props.location.pathname}>
-          <Route {...props} />
-        </div>
+        <Route {...props} />
+      </div>
 
       </CSSTransition>
-    </TransitionGroup>
+    </TransitionGroup >
   );
 })
 
@@ -146,7 +173,7 @@ const AnimationRoute = withRouter((props) => {
 const AnimationLink = withRouter((props) => {
   const { to, lock, onClick } = props;
 
-  // lock when playing animation
+  // lock the nav <a> when playing animation
   const handleClick = (e) => {
     if (lock && lock.includes(to)) e.preventDefault();
   }
