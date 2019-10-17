6146b7b849383cf91e198257465ec8cf243ddee9
xmluozp <zhaoping.luo.winnipeg@gmail.com>
2019-10-16 22:15:30 -0500

    bugs of title in mobile

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from './AboutMe.module.scss';
-import { isMobileOnly } from 'react-device-detect';
+import { isMobileOnly, isBrowser } from 'react-device-detect';
 // import LazyLoadBox,  { forceCheck }  from '../../components/LazyLoadBox/LazyLoadBox'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -49,9 +49,9 @@ export default (props) => {
 
 
             <div className={styles.popup}>
-            {isMobileOnly? null : <div className={styles.annoyingTalk}>  
+            {isBrowser? <div className={styles.annoyingTalk}>  
               Hah.  <b>What</b> is this Zhaoping? Let’s see if it is worth to <b>INSTANTIATE</b>!! ...... db.npcElite.find( {'{'}name...;
-            </div> }
+            </div> : null }
               <p>I am a web developer who has over 5 years of web development experience, skilled in both front-end and back-end.</p>
               <p>In the year 2015, I immigrated to Canada. Then I went back to school, Red River College, Winnipeg. Besides a certificate, It kept my programming knowledge up-to-date, as well as modern software industry programming standards (Agile, Git, Docker, Es-lint, Linux..etc).</p>
               <p>Recently, I graduated.</p>
